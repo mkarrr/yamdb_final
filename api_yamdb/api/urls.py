bcb7ed623e3dbbe0922a5a02c458a896b9266421
Karina Mkrtycheva <karina.mkrtycheva@gmail.com>
2022-06-26 12:39:44 +0300

    version 7

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls import include
 from rest_framework.routers import DefaultRouter
+
 from api.views import (CategoriesViewSet, CommentViewSet, GenresViewSet,
                        ReviewViewSet, TitlesViewSet, UserViewSet, jwt_token,
                        registration_user)
