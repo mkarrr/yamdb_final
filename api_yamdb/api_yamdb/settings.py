8e8306f5cbb2c9ae763bebb51da54d8ed8adb47b
Karina Mkrtycheva <karina.mkrtycheva@gmail.com>
2022-07-04 10:37:36 +0300

    version x6

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -133,7 +133,5 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'users.User'
 
-#  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-# указываем директорию, в которую будут складываться файлы писем
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
