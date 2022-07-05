717cdf2a24c4fffdd937a239a1c3c8497c6074fb
Karina Mkrtycheva <karina.mkrtycheva@gmail.com>
2022-07-05 20:35:37 +0300

    version x21

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,10 +8,8 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='NdexeTJQaohMTuTpmJkykqsRtIKP2ii9')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '51.250.18.50']
