cc3d8e388d80ac6f167621ee6396ef1eb7245568
Karina Mkrtycheva <karina.mkrtycheva@gmail.com>
2022-07-05 22:00:48 +0300

    version x27

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='NdexeTJQaohMTuTpmJkykqsRtIKP2ii9')
